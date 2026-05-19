2717fc0bd1abc04a24cb5da8ff2d96ba17baf022
dani567-wq <147517701+dani567-wq@users.noreply.github.com>
2026-05-19 19:45:02 -0300

    ok kkk

--- app.py
+++ app.py
@@ -1,8 +1,58 @@
-from fastapi import FastAPI
+from http import HTTPStatus
 
-app = FastAPI()
+from fastapi import FastAPI, HTTPException
+
+from curso_fast_api.schemas import UserDB, Userlist, UserPublic, UserSchema
+
+app = FastAPI(title='infelizmente funciona')
+
+database = []
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=dict[str, str])
 def read_root():
     return {'mensagem': 'ola mundo'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=Userlist)
+def read_users():
+    return {'users': database}
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='fudeu irmão, não tem esse usuário',
+        )
+
+    database[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}',
+    status_code=HTTPStatus.OK,
+    response_model=UserPublic,
+)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='fudeu irmão, não tem esse usuário',
+        )
+    return database.pop(user_id - 1)
